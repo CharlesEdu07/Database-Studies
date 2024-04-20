f7dfafd99642e2ca29abcf8690196fe4c7010f42
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 13:43:49 -0300

    #1 - Atualizando DaoFactory

--- tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/factory/DaoFactory.java
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/factory/DaoFactory.java
@@ -1,11 +1,29 @@
 package br.com.atividadedb.model.dao.factory;
 
 import br.com.atividadedb.db.DB;
+import br.com.atividadedb.model.dao.ActivityDao;
+import br.com.atividadedb.model.dao.DepartmentDao;
+import br.com.atividadedb.model.dao.EmployeeDao;
 import br.com.atividadedb.model.dao.ProjectDao;
+import br.com.atividadedb.model.dao.impl.ActivityDaoJDBC;
+import br.com.atividadedb.model.dao.impl.DepartmentDaoJDBC;
+import br.com.atividadedb.model.dao.impl.EmployeeDaoJDBC;
 import br.com.atividadedb.model.dao.impl.ProjectDaoJDBC;
 
 public class DaoFactory {
     public static ProjectDao createProjectDao() {
         return new ProjectDaoJDBC(DB.getConnection());
+    }
+
+    public static EmployeeDao createEmployeeDao() {
+        return new EmployeeDaoJDBC(DB.getConnection());
+    }
+
+    public static ActivityDao createActivityDao() {
+        return new ActivityDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
