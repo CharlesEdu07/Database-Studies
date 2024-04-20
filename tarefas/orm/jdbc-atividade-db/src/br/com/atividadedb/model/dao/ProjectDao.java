20b485c9638486fe5d5ed72c95af4be248614f8a
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 13:43:23 -0300

    #1 - Adicionando função inserir para projeto

--- tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/ProjectDao.java
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/ProjectDao.java
@@ -2,8 +2,11 @@ package br.com.atividadedb.model.dao;
 
 import br.com.atividadedb.model.entities.Activity;
 import br.com.atividadedb.model.entities.Employee;
+import br.com.atividadedb.model.entities.Project;
 
 public interface ProjectDao {
+    void insert(Project project);
+
     void addActivity(Activity activity);
 
     void updateResponsible(Employee employee);
--- tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/ProjectDaoJDBC.java
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/ProjectDaoJDBC.java
@@ -1,8 +1,15 @@
 package br.com.atividadedb.model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import br.com.atividadedb.db.DB;
+import br.com.atividadedb.db.DbException;
 import br.com.atividadedb.model.dao.ProjectDao;
+import br.com.atividadedb.model.entities.Project;
 import br.com.atividadedb.model.entities.Activity;
 import br.com.atividadedb.model.entities.Employee;
 
@@ -14,9 +21,46 @@ public class ProjectDaoJDBC implements ProjectDao {
     }
 
     @Override
+    public void insert(Project project) {
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO atividade (nome, descricao, responsavel, depto, data_inicio, data_fim) VALUES (?, ?, ?, ?, ?, ?)");
+
+            st.setString(1, project.getName());
+            st.setString(2, project.getDescription());
+            st.setLong(3, project.getResponsible().getId());
+            st.setLong(4, project.getDepartment().getId());
+            st.setDate(5, new Date(project.getStartDate().getTime()));
+            st.setDate(6, new Date(project.getEndDate().getTime()));
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+
+                if (rs.next()) {
+                    long id = rs.getLong(1);
+
+                    project.setId(id);
+                }
+
+                DB.closeResultSet(rs);
+            } else {
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
     public void addActivity(Activity activity) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insertActivity'");
+        throw new UnsupportedOperationException("Unimplemented method 'addActivity'");
     }
 
     @Override
