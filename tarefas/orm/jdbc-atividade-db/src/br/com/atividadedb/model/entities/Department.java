3511f9695428ee5864665e5844750463ee3f3e52
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 10:25:08 -0300

    #1 - Criando entidade para o departamento

---
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/entities/Department.java
@@ -0,0 +1,83 @@
+package br.com.atividadedb.model.entities;
+
+public class Department {
+    private Long id;
+    private String name;
+    private String acronym;
+    private String description;
+    private Long manager;
+
+    public Department() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getManager() {
+        return manager;
+    }
+
+    public void setManager(Long manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Department other = (Department) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [id=" + id + ", name=" + name + ", acronym=" + acronym + ", description=" + description
+                + ", manager=" + manager + "]";
+    }
+}
