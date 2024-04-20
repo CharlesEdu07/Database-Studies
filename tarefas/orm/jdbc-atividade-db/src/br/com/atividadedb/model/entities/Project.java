fdaeb796dc5caf0ddc8d89f882db1ca190780484
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 10:37:56 -0300

    #1 - Criando entidade para projeto

---
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/entities/Project.java
@@ -0,0 +1,103 @@
+package br.com.atividadedb.model.entities;
+
+import java.time.LocalDate;
+
+public class Project {
+    private Long id;
+    private String name;
+    private String description;
+    private Employee responsible;
+    private Department department;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Employee getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Employee responsible) {
+        this.responsible = responsible;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Project other = (Project) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Project [id=" + id + ", name=" + name + ", description=" + description + ", responsible=" + responsible
+                + ", department=" + department + ", startDate=" + startDate + ", endDate=" + endDate + "]";
+    }
+}
