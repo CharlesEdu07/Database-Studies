80426050767a6097de2e5a8413eed4f0df575832
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 19:39:55 -0300

    #1 - Implementando findById na implementação do funcionário

--- tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/EmployeeDaoJDBC.java
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/EmployeeDaoJDBC.java
@@ -1,5 +1,6 @@
 package br.com.atividadedb.model.dao.impl;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -24,7 +25,7 @@ public class EmployeeDaoJDBC implements EmployeeDao {
 
         try {
             st = conn.prepareStatement(
-                    "INSERT INTO employee (nome, sexo, dt_nasc, salario, supervisor, depto) VALUES (?, ?, ?, ?, ?, ?)");
+                    "INSERT INTO funcionario (nome, sexo, dt_nasc, salario, supervisor, depto) VALUES (?, ?, ?, ?, ?, ?)");
 
             st.setString(1, employee.getName());
             st.setString(2, employee.getGender().toString());
@@ -53,5 +54,50 @@ public class EmployeeDaoJDBC implements EmployeeDao {
         } finally {
             DB.closeStatement(st);
         }
+    }
+
+    @Override
+    public Employee findById(Long id) {
+        PreparedStatement st = null;
+
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT * FROM funcionario WHERE codigo = ?");
+
+            st.setLong(1, id);
+
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Employee dep = instantiateEmployee(rs);
+
+                return dep;
+            }
+
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Employee instantiateEmployee(ResultSet rs) throws SQLException {
+        Employee emp = new Employee();
+
+        emp.setId(rs.getLong("codigo"));
+        emp.setName(rs.getString("nome"));
+        emp.setGender(rs.getString("sexo").charAt(0));
+        emp.setBirthDate(rs.getDate("dt_nasc"));
+
+        String salaryString = rs.getString("salario").replaceAll("[^0-9.]", "");
+
+        BigDecimal salary = new BigDecimal(salaryString);
+
+        emp.setSalary(salary);
+
+        return emp;
     }
 }
