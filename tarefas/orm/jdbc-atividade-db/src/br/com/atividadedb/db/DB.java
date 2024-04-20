61e9e4918650e7e03b194b5bf9a80d8ee603952c
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 09:29:43 -0300

    #1 - Função para obter conexão

---
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/db/DB.java
@@ -0,0 +1,38 @@
+package br.com.atividadedb.db;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+
+                String url = props.getProperty("dburl");
+
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return conn;
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+
+            return props;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
