350c6f6b1f0008c95d9dc45b9dc22a8b284d009a
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 11:37:47 -0300

    Criando EmployeeDaoJDBC para implementação de métodos com conexão JDBC

---
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/EmployeeDaoJDBC.java
@@ -0,0 +1,13 @@
+package br.com.atividadedb.model.dao.impl;
+
+import br.com.atividadedb.model.dao.EmployeeDao;
+import br.com.atividadedb.model.entities.Employee;
+
+public class EmployeeDaoJDBC implements EmployeeDao {
+
+    @Override
+    public void insert(Employee employee) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+}
