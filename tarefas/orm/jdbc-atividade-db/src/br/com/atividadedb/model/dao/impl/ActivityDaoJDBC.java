75e9130c2aa5f8b3c26183e8a2b7aeaa694b5a01
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 11:37:32 -0300

    Criando ActivityDaoJDBC para implementação de métodos com conexão JDBC

---
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/model/dao/impl/ActivityDaoJDBC.java
@@ -0,0 +1,13 @@
+package br.com.atividadedb.model.dao.impl;
+
+import br.com.atividadedb.model.dao.ActivityDao;
+import br.com.atividadedb.model.entities.Activity;
+
+public class ActivityDaoJDBC implements ActivityDao {
+
+    @Override
+    public void insert(Activity activity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+}
