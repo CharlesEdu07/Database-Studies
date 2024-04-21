85255ff1e2003c47887aa5e1d9de47a03c3174c4
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-21 17:44:11 -0300

    #1 - Adicionando lista de atividades

--- tarefas/orm/orm-atividade-db/src/main/java/br/com/charlesedu/atividadedb/model/Project.java
+++ tarefas/orm/orm-atividade-db/src/main/java/br/com/charlesedu/atividadedb/model/Project.java
@@ -2,23 +2,22 @@ package br.com.charlesedu.atividadedb.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "projeto")
 public class Project implements Serializable {
@@ -49,4 +48,117 @@ public class Project implements Serializable {
     @Column(name = "data_fim")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
+    @OneToMany(mappedBy = "project", fetch = FetchType.EAGER)
+    private List<Activity> activities = new ArrayList<>();
+
+    public Project() {
+    }
+
+    public Project(Long id, String name, String description, Employee responsible, Department department,
+            LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.responsible = responsible;
+        this.department = department;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Employee getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Employee responsible) {
+        this.responsible = responsible;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Project other = (Project) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Project [id=" + id + ", name=" + name + ", description=" + description + ", responsible=" + responsible
+                + ", department=" + department + ", startDate=" + startDate + ", endDate=" + endDate + ", activities="
+                + activities + "]";
+    }
 }
