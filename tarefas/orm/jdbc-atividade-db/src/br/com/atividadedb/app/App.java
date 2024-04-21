957c39cd796c2a4d673935f9fbe7bfadcb3eec90
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-20 21:44:34 -0300

    #1 - Código teste para inserir atividade, alterar responsável e buscar todos os projetos com suas respectivas atividades

--- tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/app/App.java
+++ tarefas/orm/jdbc-atividade-db/src/br/com/atividadedb/app/App.java
@@ -5,45 +5,90 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import br.com.atividadedb.model.dao.ActivityDao;
 import br.com.atividadedb.model.dao.DepartmentDao;
 import br.com.atividadedb.model.dao.EmployeeDao;
 import br.com.atividadedb.model.dao.ProjectDao;
 import br.com.atividadedb.model.dao.factory.DaoFactory;
+import br.com.atividadedb.model.entities.Activity;
 import br.com.atividadedb.model.entities.Department;
 import br.com.atividadedb.model.entities.Employee;
 import br.com.atividadedb.model.entities.Project;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Locale.setDefault(Locale.US);
+        public static void main(String[] args) throws Exception {
+                Locale.setDefault(Locale.US);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        EmployeeDao employeeDao = DaoFactory.createEmployeeDao();
-        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-        ProjectDao projectDao = DaoFactory.createProjectDao();
+                EmployeeDao employeeDao = DaoFactory.createEmployeeDao();
+                DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+                ProjectDao projectDao = DaoFactory.createProjectDao();
+                ActivityDao activityDao = DaoFactory.createActivityDao();
 
-        System.out.println("\n=== TEST 1: Insert Employee ===");
+                // =========== Insert Employee ===========
 
-        Employee existingEmployee = employeeDao.findById(1L);
+                System.out.println("\n=== TEST 1: Insert Employee ===");
 
-        Department existingDepartment = departmentDao.findById(1L);
+                Employee existingEmployee = employeeDao.findById(1L);
 
-        Employee employee1 = new Employee(null, "Carlos", 'M', new Date(sdf.parse("01/01/1990").getTime()),
-                new BigDecimal(3000.00), existingEmployee, existingDepartment);
+                Department existingDepartment = departmentDao.findById(1L);
 
-        employeeDao.insert(employee1);
+                Employee employee1 = new Employee(null, "Carlos", 'M', new Date(sdf.parse("01/01/1990").getTime()),
+                                new BigDecimal(3000.00), existingEmployee, existingDepartment);
 
-        System.out.println("Inserted employee! ID: " + employee1.getId());
+                employeeDao.insert(employee1);
 
-        System.out.println("\n=== TEST 2: Insert Project ===");
+                System.out.println("Inserted employee! ID: " + employee1.getId());
 
-        Project project1 = new Project(null, "Projeto teste", "Description of the new project", employee1,
-                existingDepartment, new Date(sdf.parse("01/01/2021").getTime()),
-                new Date(sdf.parse("01/01/2022").getTime()));
+                // =========== Insert Project ===========
 
-        projectDao.insert(project1);
+                System.out.println("\n=== TEST 2: Insert Project ===");
 
-        System.out.println("Inserted project! ID: " + project1.getId());
-    }
+                Project project1 = new Project(null, "Projeto de BD", "Description of the new project", employee1,
+                                existingDepartment, new Date(sdf.parse("01/01/2021").getTime()),
+                                new Date(sdf.parse("01/01/2022").getTime()));
+
+                projectDao.insert(project1);
+
+                System.out.println("\nInserted project! ID: " + project1.getId());
+
+                // =========== Insert Activity ===========
+
+                System.out.println("\n=== TEST 3: Insert an Activity ===");
+
+                Project existingProject = projectDao.findById(1L);
+
+                System.out.println("\nProject: " + existingProject);
+
+                Activity activity1 = new Activity(null, "Atividade de teste", existingProject,
+                                new Date(sdf.parse("01/01/2021").getTime()),
+                                new Date(sdf.parse("01/01/2022").getTime()));
+
+                activityDao.insert(activity1);
+
+                System.out.println("\nInserted activity! ID: " + activity1.getId());
+
+                // =========== Update Responsible ===========
+
+                System.out.println("\n=== TEST 4: Update Responsible ===");
+
+                Employee employee2 = employeeDao.findById(3L);
+
+                projectDao.updateResponsible(project1, employee2);
+
+                System.out.println("\nUpdated responsible!");
+
+                // =========== Find All Projects and respective activities ===========
+
+                System.out.println("\n=== TEST 5: Find All Projects and respective activities ===");
+
+                for (Project project : projectDao.findAllWithActivities().keySet()) {
+                        System.out.println("\n" + project);
+
+                        for (Activity activity : projectDao.findAllWithActivities().get(project)) {
+                                System.out.println(activity);
+                        }
+                }
+        }
 }
